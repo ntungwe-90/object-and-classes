6228f338f52c80270f0c9b3545bf83667e2d1931
irene <ntongweirene@gmail.com>
2020-05-11 18:01:56 -0700

    first encounter with objects and classes

---
+++ obj.js
@@ -0,0 +1,16 @@
+class animal{
+
+constructor(name,height,color,age){
+ 
+ this.name = 'name',
+ this.height = 'height',
+ this.color = 'black',
+ this.age = 'age'
+
+}
+ }
+ let pig = new animal("pig",1.0,"yello",5)
+ let dog = new animal('dog',0.7,'brown',10)
+
+ console.log(pig);
+ console.log(dog);
